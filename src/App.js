b9ee80385a18bd8abb0dadb1abf927bd94a4622f
Maayan Albert <maayan@Maayans-MBP.fios-router.home>
2020-01-08 18:13:17 -0500

    added modal to get ppl on chrome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.scss';
+import "./App.scss";
 
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 
-import View from "./View.js"
-import Modal from 'react-bootstrap/Modal'
-import BrowserDetection from 'react-browser-detection';
+import View from "./View.js";
+import Modal from "react-bootstrap/Modal";
+const { detect } = require('detect-browser');
+const browser = detect();
 
 var userAgent = navigator.userAgent.toLowerCase();
 if (userAgent.indexOf(" electron/") > -1) {
@@ -14,31 +15,38 @@ if (userAgent.indexOf(" electron/") > -1) {
   var ipc = false;
 }
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalVisible: !ipc && browser && browser.name != "chrome" };
+  }
 
-function App() {
-const [show, setShow] = useState(!ipc);
-const handleClose = () => {setShow(false); console.log(show)};
-const browserModal = {
-  chrome: () => null,
-  default: () => 
-      <Modal show={show} style={{zIndex:2000000000000000001}} centered onHide={handleClose}>
-        <Modal.Header closeButton>
-     <Modal.Title  className="name">Woohoo, you're reading this text in a modal!</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="picker">Woohoo, you're reading this text in a modal!</Modal.Body>
-      </Modal>,
-};
+  getModalVisible(){
+  	console.log(this.state.modalVisible)
+  	return this.state.modalVisible
+  }
 
-  return (
-    <div>
-      <BrowserDetection>
-        { browserModal }
-      </BrowserDetection>
 
-    	<View />
-    </div>
-  );
 
+
+  render() {
+    return (
+      <div>
+        <Modal
+          show={this.state.modalVisible}
+          style={{ zIndex: 2000000000000000001 }}
+          centered
+          onHide={() => this.setState({ modalVisible: false }) }
+        >
+          <Modal.Header closeButton>
+            <Modal.Title className="name">Use Chrome please!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="picker">
+            Right now, Stamper is only supported in Google Chrome.{" "}
+          </Modal.Body>
+        </Modal>
+        <View />
+      </div>
+    );
+  }
 }
-
-export default App;
