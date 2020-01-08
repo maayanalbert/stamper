4527b513cc8c19f82acf9c6c54a1ff2495cea394
Maayan Albert <maayan@Maayans-MBP.fios-router.home>
2020-01-07 23:07:42 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.039a02d0ca07f29097849924611fce43.js"
+  "./precache-manifest.891faa080ae5fa5e5cd228fe82df8b68.js"
 );
 
 self.addEventListener('message', (event) => {
