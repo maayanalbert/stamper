639b56cfa13649283642d53d2712232be3c28397
Maayan Albert <maayan@Maayans-MacBook-Pro.local>
2020-02-09 21:47:12 -0800

    trying to figure out what's up with console resize

--- src/ConsoleStamp.js
+++ src/ConsoleStamp.js
@@ -6,7 +6,6 @@ import AceEditor from "react-ace";
 import pf, { globals, p5Lib } from "./globals.js";
 import { Hook, Console, Decode } from "console-feed";
 
-
 var _ = require("lodash");
 
 var userAgent = navigator.userAgent.toLowerCase();
@@ -20,6 +19,7 @@ if (userAgent.indexOf(" electron/") > -1) {
 export default class ConsoleStamp extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       logs: [],
       consoleHeight: this.props.starterConsoleHeight,
@@ -28,33 +28,33 @@ export default class ConsoleStamp extends Component {
       hidden: this.props.initialHidden,
       x: this.props.initialPosition.x,
       y: this.props.initialPosition.y,
-      zIndex:-1
+      zIndex: -1
     };
 
     this.cristalRef = React.createRef();
-    this.limit = 100
-    this.receiveMessage = this.receiveMessage.bind(this)
+    this.limit = 100;
+    this.receiveMessage = this.receiveMessage.bind(this);
   }
 
-  receiveMessage(e){
-     if(e.data.type != "error"){
-        return
-      }
+  receiveMessage(e) {
+    if (e.data.type != "error") {
+      return;
+    }
 
-      this.logToConsole(e.data.message);
+    this.logToConsole(e.data.message);
 
-      var lineNum = e.data.lineno;
-      var message = e.data.message;
-      var id = e.data.id;
+    var lineNum = e.data.lineno;
+    var message = e.data.message;
+    var id = e.data.id;
 
-      this.props.addErrorLine(lineNum, id);
+    this.props.addErrorLine(lineNum, id);
   }
 
   componentDidMount() {
     window.addEventListener("message", this.receiveMessage);
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     window.removeEventListener("message", this.receiveMessage);
   }
 
@@ -69,16 +69,20 @@ export default class ConsoleStamp extends Component {
   toggleHide(callback) {
     ipc && ipc.send("edited");
     if (this.state.hidden) {
-      this.setState({ hidden: false, savedIframeHeight:this.state.iframeHeight, iframeHeight:0 }, 
-        () => this.setState({iframeHeight:this.state.savedIframeHeight}, callback)
-
-      )
-
-    } else {
       this.setState(
-        { hidden: true},
-        callback
+        {
+          hidden: false,
+          savedIframeHeight: this.state.iframeHeight,
+          iframeHeight: 0
+        },
+        () =>
+          this.setState(
+            { iframeHeight: this.state.savedIframeHeight },
+            callback
+          )
       );
+    } else {
+      this.setState({ hidden: true }, callback);
     }
   }
   setEditorScrolling(isScrolling) {
@@ -86,17 +90,20 @@ export default class ConsoleStamp extends Component {
   }
 
   addNewIframeConsole(newConsole) {
-
     var p5LogError = `Did you just try to use p5.js's loop() function? If so, you may want to move it into your sketch's setup() function.
 
-For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#why-cant-i-assign-variables-using-p5-functions-and-variables-before-setup`
+For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#why-cant-i-assign-variables-using-p5-functions-and-variables-before-setup`;
 
-  var p5LogError2 = `Did you just try to use p5.js's noLoop() function? If so, you may want to move it into your sketch's setup() function.
+    var p5LogError2 = `Did you just try to use p5.js's noLoop() function? If so, you may want to move it into your sketch's setup() function.
 
-For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#why-cant-i-assign-variables-using-p5-functions-and-variables-before-setup`
+For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#why-cant-i-assign-variables-using-p5-functions-and-variables-before-setup`;
     Hook(newConsole, newLogs => {
       newLogs.map(log => {
-        if (log.method === "log" && log.data[0] != p5LogError && log.data[0] != p5LogError2) {
+        if (
+          log.method === "log" &&
+          log.data[0] != p5LogError &&
+          log.data[0] != p5LogError2
+        ) {
           this.checkLastLog(log);
         }
       });
@@ -104,31 +111,27 @@ For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#w
   }
 
   checkLastLog(log) {
- 
-
-
     var logs = this.state.logs;
     if (logs.length < 1) {
       this.setState({ logs: [log], lastFreq: 0 });
       var consoleContainer = document.getElementById("consoleContainer");
-      if(consoleContainer){
+      if (consoleContainer) {
         consoleContainer.scrollTop = consoleContainer.scrollHeight;
       }
-  
     } else {
       var lastLog = logs[logs.length - 1];
 
       if (_.isEqual(lastLog.data, log.data)) {
-        if(this.state.lastFreq >= this.limit){
-          return
+        if (this.state.lastFreq >= this.limit) {
+          return;
         }
         this.setState({ lastFreq: this.state.lastFreq + 1 });
       } else {
         if (this.state.lastFreq > 1) {
-                var freqString = this.state.lastFreq.toString() 
-      if(this.state.lastFreq >= this.limit){
-        freqString += "+"
-      }
+          var freqString = this.state.lastFreq.toString();
+          if (this.state.lastFreq >= this.limit) {
+            freqString += "+";
+          }
           logs.push({
             method: "command",
             data: ["(" + freqString + ")"]
@@ -137,16 +140,14 @@ For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#w
         logs.push(log);
         this.setState({ logs: logs, lastFreq: 1 });
         var consoleContainer = document.getElementById("consoleContainer");
-        if(consoleContainer){
+        if (consoleContainer) {
           consoleContainer.scrollTop = consoleContainer.scrollHeight;
         }
-
       }
     }
   }
 
   logToConsole(message, method = "error") {
-
     this.checkLastLog({ method: method, data: [message] });
   }
 
@@ -157,47 +158,38 @@ For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#w
       consoleWidth: this.state.consoleWidth,
       consoleHeight: this.state.consoleHeight,
       hidden: this.state.hidden,
-      zIndex:this.state.zIndex
+      zIndex: this.state.zIndex
     };
 
     return data;
   }
 
-  resizeConsole(widthDiff, heightDiff) {
-    var height = this.state.consoleHeight;
-    var width = this.state.consoleWidth;
-    this.setState({
-      consoleHeight: height + heightDiff,
-      consoleWidth: width + widthDiff
-    });
-      var consoleContainer = document.getElementById("consoleContainer");
-      if(consoleContainer){
-        consoleContainer.scrollTop = consoleContainer.scrollHeight;
-      }
-  }
-
-    resizeConsole(widthDiff, heightDiff, x) {
-    var height = this.state.consoleHeight + heightDiff;
-    var width = this.state.consoleWidth + widthDiff;
+  resizeConsole(widthDiff, heightDiff, x, change = true) {
+    var height = this.state.consoleWidth + heightDiff;
+    var width = this.state.consoleHeight + widthDiff;
 
     if (height < 0 || width < 0) {
       return true;
     }
 
-    this.setState({
-      consoleHeight: height,
-      consoleWidth: width, 
-      x:x
-    });
-    // this.editorRef.current.editor.resize();
+    console.log("WIDTH", this.state.consoleWidth);
+    // console.log("HEIGHT", this.state.consoleHeight);
+
+    if (change) {
+      this.setState({
+        consoleHeight: height,
+        consoleWidth: width,
+        x: x
+      });
+    }
   }
 
   render() {
     var renderedLogs = _.cloneDeep(this.state.logs);
     if (this.state.lastFreq > 1) {
-      var freqString = this.state.lastFreq.toString() 
-      if(this.state.lastFreq >= this.limit){
-        freqString += "+"
+      var freqString = this.state.lastFreq.toString();
+      if (this.state.lastFreq >= this.limit) {
+        freqString += "+";
       }
 
       renderedLogs.push({
@@ -205,10 +197,10 @@ For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#w
         data: ["(" + freqString + ")"]
       });
 
-        var consoleContainer = document.getElementById("consoleContainer");
-        if(consoleContainer){
-          consoleContainer.scrollTop = consoleContainer.scrollHeight;
-        }      
+      var consoleContainer = document.getElementById("consoleContainer");
+      if (consoleContainer) {
+        consoleContainer.scrollTop = consoleContainer.scrollHeight;
+      }
     }
 
     if (this.state.hidden) {
@@ -217,26 +209,28 @@ For more details, see: https://github.com/processing/p5.js/wiki/p5.js-overview#w
 
     return (
       <Cristal
-             getLinesOn={this.props.getLinesOn}
-                getSnapMargin={this.props.getSnapMargin}
-           zIndex={this.props.starterZIndex}
+        getLinesOn={this.props.getLinesOn}
+        getSnapMargin={this.props.getSnapMargin}
+        zIndex={this.props.starterZIndex}
         getScale={this.props.getScale}
-       parentID = {this.props.id}
+        parentID={this.props.id}
         ref={this.cristalRef}
         closeHidden={true}
         copyHidden={true}
         onResize={this.resizeConsole.bind(this)}
         initialPosition={{ x: this.state.x, y: this.state.y }}
         onMove={s => this.setState({ x: s.x, y: s.y })}
-                  onStartResize={this.props.onStartMove.bind(this)}
-          onStopResize={this.props.onStopMove.bind(this)}
+        onStartResize={this.props.onStartMove.bind(this)}
+        onStopResize={this.props.onStopMove.bind(this)}
         className="stamp bg-lightGrey border border-borderGrey shadow-sm"
         icon={this.getIcon()}
         showClear
-                  onZChange={s => this.setState({zIndex:s.zIndex})}
+        onZChange={s => this.setState({ zIndex: s.zIndex })}
         onClear={this.clearConsole.bind(this)}
-        onResize={this.resizeConsole.bind(this)}
-                  initialSize={{width:this.state.consoleWidth + 22, height:this.state.consoleHeight + 50 }}
+        initialSize={{
+          width: this.state.consoleWidth + 22,
+          height: this.state.consoleHeight + 50
+        }}
       >
         <div
           id="consoleContainer"
